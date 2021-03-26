bae1a9861c70a25f5a7cbc70b98fd2bd0aadcd67
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-03-26 00:39:03 -0300

    Done: Should validate radio-button groups

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -36,7 +36,7 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
-    it.only("TextFields", () => {
+    it("TextFields", () => {
         cy.get('#formNome').type('Cypress Test')
         cy.get('#formNome').should('have.value', 'Cypress Test')
 
@@ -56,5 +56,17 @@ describe('Work with basic elements', () => {
             .clear()
             .type('Erro{selectall}acerto', { delay: 100})
             .should('have.value', 'acerto')
+    })
+
+    it.only('RadioButton', () => {
+        cy.get('#formSexoFem')
+            .click()
+            .should('be.checked')
+
+        cy.get('#formSexoMasc')
+            .should('not.be.checked')
+
+        cy.get("[name='formSexo']")
+        .should('have.length', 2)
     })
 })

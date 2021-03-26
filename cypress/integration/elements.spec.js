8b1c8940e10b44d66ee2b3977bbc3932bdef2c72
Luciano dii Souza <lucianodiisouza@hotmail.com>
2021-03-26 00:31:36 -0300

    done: Should use textfields (textarea, inputs)

--- cypress/integration/elements.spec.js
+++ cypress/integration/elements.spec.js
@@ -36,7 +36,25 @@ describe('Work with basic elements', () => {
         cy.get('#resultado').should('have.text', 'Voltou!')
     })
 
-    it("Hooks", () => {
+    it.only("TextFields", () => {
+        cy.get('#formNome').type('Cypress Test')
+        cy.get('#formNome').should('have.value', 'Cypress Test')
 
+        cy.get('#elementosForm\\:sugestoes')
+            .type('Textarea Test')
+            .should('have.value', 'Textarea Test')
+
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+            .type('Luciano')
+            .should('have.value', 'Luciano')
+
+        cy.get('[data-cy=dataSobrenome]')
+            .type('Teste12345{backspace}{backspace}')
+            .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', { delay: 100})
+            .should('have.value', 'acerto')
     })
 })
